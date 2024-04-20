825705371daef72fd5f7e2b646925cee1ae0d9e9
honjow <honjow311@gmail.com>
2024-04-21 03:15:32 +0800

    add --custom-refresh-rates

--- src/drm.cpp
+++ src/drm.cpp
@@ -523,6 +523,8 @@ bool g_bSupportsSyncObjs = false;
 extern gamescope::GamescopeModeGeneration g_eGamescopeModeGeneration;
 extern GamescopePanelOrientation g_DesiredInternalOrientation;
 
+extern std::vector<uint32_t> g_customRefreshRates;
+
 extern GamescopePanelType g_eGamescopePanelType;
 extern GamescopePanelExternalOrientation g_eGamescopePanelExternalOrientation;
 
@@ -2115,6 +2117,10 @@ namespace gamescope
 			if (fr.size()) {
 				m_Mutable.ValidDynamicRefreshRates = fr;
 			}
+		}
+		else if ( !g_customRefreshRates.empty() )
+		{
+			m_Mutable.ValidDynamicRefreshRates = std::span( g_customRefreshRates );
 		}
 		else if ( bSteamDeckDisplay )
 		{
